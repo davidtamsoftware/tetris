8a7ac26d114b5f3c8437e9447144109fff318c2b
davidtamsoftware <david.tam.kc@gmail.com>
2018-11-04 16:37:34 -0800

    Add block component

---
+++ packages/tetris/src/Block.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import './Block.css';
+
+interface Props {
+    data: number;
+    size?: "small" | "large";
+}
+
+const colors = {
+    "1": "orange #c88a19 darkorange orange",
+    "2": "yellow #ebeb5e #afaf59 yellow",
+    "3": "green lightgreen darkgreen green",
+    "4": "blue lightblue darkblue blue",
+    "5": "gray lightgray darkgray gray",
+    "6": "red brown darkred red",
+    "7": "purple #892289 #5a065a purple"
+  };
+
+export const Block = (props: Props) => (
+    <div 
+        style={{
+            border: `${props.size === "small" ? "2":"5"}px solid`,
+            borderRadius: "2px",
+            borderColor: colors[props.data],
+            backgroundColor: colors[props.data].split(" ")[0],
+            width: `${props.size === "small" ? "5":"15"}px`,
+            height: `${props.size === "small" ? "5":"15"}px`,
+            padding: "0",
+        }}/>
+);
