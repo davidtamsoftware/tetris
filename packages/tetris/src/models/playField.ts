214ebfad56887840702cc94cca5c375764a918d4
davidtamsoftware <david.tam.kc@gmail.com>
2018-11-09 14:03:04 -0800

    Rename file

--- packages/tetris/src/models/playField.ts
+++
@@ -1,7 +0,0 @@
-export type PlayField = number[][];
-
-export const generatePlayField = (row: number, col: number) => new Array<number[]>(row).fill(new Array<number>(col).fill(0));
-
-export const playField: PlayField = generatePlayField(18, 10);
-
-export default PlayField;
