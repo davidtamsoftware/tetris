ffb260c544462b66ac5a63a3067bf9b198574dd3
davidtamsoftware <david.tam.kc@gmail.com>
2018-11-09 11:07:07 -0800

    update init values for play field size

--- packages/tetris/src/models/playField.ts
+++ packages/tetris/src/models/playField.ts
@@ -1,8 +1,5 @@
-import { number } from 'prop-types';
-
-// 10W X 18H
 export type PlayField = number[][];
 
 export const generatePlayField = (row: number, col: number) => new Array<number[]>(row).fill(new Array<number>(col).fill(0));
 
-export const playField: PlayField = generatePlayField(18, 30);
+export const playField: PlayField = generatePlayField(18, 10);
