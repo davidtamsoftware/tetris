be84c841732e681ee517690b86d12bc0c7291fbf
davidtamsoftware <david.tam.kc@gmail.com>
2018-11-12 08:58:00 -0800

    Rename PlayField to Playfield

---
+++ packages/tetris/src/models/Playfield.ts
@@ -0,0 +1,7 @@
+export type PlayField = number[][];
+
+export const generatePlayField = (row: number, col: number) => new Array<number[]>(row).fill(new Array<number>(col).fill(0));
+
+export const playField: PlayField = generatePlayField(18, 10);
+
+export default PlayField;
