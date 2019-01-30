b5874cd960369635a9cfd7a7f99632fb24dbb3af
davidtamsoftware <david.tam.kc@gmail.com>
2019-01-29 21:04:29 -0800

    Unit tests

--- packages/tetris-core/src/actions/__tests__/Multiplayer.test.ts
+++ packages/tetris-core/src/actions/__tests__/Multiplayer.test.ts
@@ -1,7 +1,115 @@
-xdescribe("High score battle logic", () => {
-    it("should declare the winner when both player's games end");
-    it("should generate the same sequence of pieces");
-    it("should be able to restart the game when game is over");
+import { GameState } from "../../models";
+import { Multiplayer } from "../../tetris-core";
+import { MultiplayerMode, Player } from "../Multiplayer";
+
+describe("High score battle logic", () => {
+    it("should declare the winner when both player's games end", async () => {
+        const multiplayer = new Multiplayer.Multiplayer(MultiplayerMode.HighScoreBattle);
+        multiplayer.start();
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+        // worst case is 18 pieces to force game over
+        // height is 18, smallest piece is 1 row tall
+        for (let i = 0; i < 18; i++) {
+            await multiplayer.drop(Multiplayer.Player.One, true);
+            await multiplayer.drop(Multiplayer.Player.Two, true);
+        }
+        expect(multiplayer.getState().gameState).toBe(GameState.GameOver);
+        expect(multiplayer.getState().winner).toBeNull();
+
+        multiplayer.restart();
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+
+        // worst case is 18 pieces to force game over
+        // height is 18, smallest piece is 1 row tall
+        for (let i = 0; i < 18; i++) {
+            await multiplayer.drop(Multiplayer.Player.One, true);
+        }
+
+        await new Promise((res, rej) => setTimeout(res, 2000));
+        // worst case is 18 pieces to force game over
+        // height is 18, smallest piece is 1 row tall
+        for (let i = 0; i < 18; i++) {
+            await multiplayer.drop(Multiplayer.Player.Two, true);
+        }
+
+        expect(multiplayer.getState().gameState).toBe(GameState.GameOver);
+        expect(multiplayer.getState().winner).toBe(Multiplayer.Player.One);
+    });
+
+    it("should generate the same sequence of pieces", async () => {
+        const multiplayer = new Multiplayer.Multiplayer(MultiplayerMode.HighScoreBattle);
+        multiplayer.start();
+        const history = [];
+        // simulate that player 1 played faster
+        history.push(multiplayer.getState().player1.piece);
+        await multiplayer.drop(Multiplayer.Player.One, true);
+        history.push(multiplayer.getState().player1.piece);
+        await multiplayer.drop(Multiplayer.Player.One, true);
+        history.push(multiplayer.getState().player1.piece);
+        await multiplayer.drop(Multiplayer.Player.One, true);
+        history.push(multiplayer.getState().player1.piece);
+
+        // check that player 2 got the same pieces as player 1 and simulate
+        // that player 2 caught up and played faster and got ahead by 2 pieces
+        expect(multiplayer.getState().player2.piece).toEqual(history.shift());
+        await multiplayer.drop(Multiplayer.Player.Two, true);
+        expect(multiplayer.getState().player2.piece).toEqual(history.shift());
+        await multiplayer.drop(Multiplayer.Player.Two, true);
+        expect(multiplayer.getState().player2.piece).toEqual(history.shift());
+        await multiplayer.drop(Multiplayer.Player.Two, true);
+        expect(multiplayer.getState().player2.piece).toEqual(history.shift());
+        await multiplayer.drop(Multiplayer.Player.Two, true);
+        history.push(multiplayer.getState().player2.piece);
+        await multiplayer.drop(Multiplayer.Player.Two, true);
+        history.push(multiplayer.getState().player2.piece);
+
+        // check that player 1 got the same pieces as player 2
+        await multiplayer.drop(Multiplayer.Player.One, true);
+        expect(multiplayer.getState().player1.piece).toEqual(history.shift());
+        await multiplayer.drop(Multiplayer.Player.One, true);
+        expect(multiplayer.getState().player1.piece).toEqual(history.shift());
+    });
+
+    it("should be able to restart the game when game is over", async () => {
+        const multiplayer = new Multiplayer.Multiplayer(MultiplayerMode.HighScoreBattle);
+        multiplayer.start();
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+        // worst case is 18 pieces to force game over
+        // height is 18, smallest piece is 1 row tall
+        for (let i = 0; i < 18; i++) {
+            await multiplayer.drop(Multiplayer.Player.One, true);
+            await multiplayer.drop(Multiplayer.Player.Two, true);
+        }
+        expect(multiplayer.getState().gameState).toBe(GameState.GameOver);
+        expect(multiplayer.getState().winner).toBeNull();
+
+        multiplayer.restart();
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+        expect(multiplayer.getState().player1.playfield.filter((line) => line.every((cell) => !!cell))).toEqual([]);
+        expect(multiplayer.getState().player2.playfield.filter((line) => line.every((cell) => !!cell))).toEqual([]);
+    });
+
+    it("should be able to pause/unpause the game", async () => {
+        const multiplayer = new Multiplayer.Multiplayer(MultiplayerMode.HighScoreBattle);
+        multiplayer.start();
+        await multiplayer.drop(Player.Two, true);
+        await multiplayer.drop(Player.Two);
+        await new Promise((res, rej) => setTimeout(res, 100));
+        const prevPlayer1Piece = multiplayer.getState().player1.piece;
+        const prevPlayer1Position = multiplayer.getState().player1.position;
+        const prevPlayer2Piece = multiplayer.getState().player2.piece;
+        const prevPlayer2Position = multiplayer.getState().player2.position;
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+        multiplayer.togglePause();
+        expect(multiplayer.getState().gameState).toBe(GameState.Paused);
+        await new Promise((res, rej) => setTimeout(res, 2000));
+        expect(prevPlayer1Piece).toEqual(multiplayer.getState().player1.piece);
+        expect(prevPlayer1Position).toEqual(multiplayer.getState().player1.position);
+        expect(prevPlayer2Piece).toEqual(multiplayer.getState().player2.piece);
+        expect(prevPlayer2Position).toEqual(multiplayer.getState().player2.position);
+        multiplayer.togglePause();
+        expect(multiplayer.getState().gameState).toBe(GameState.Active);
+    });
 });
 
 xdescribe("Attack mode logic", () => {
--- packages/tetris-core/src/actions/__tests__/Tetris.test.ts
+++ packages/tetris-core/src/actions/__tests__/Tetris.test.ts
@@ -1,7 +1,7 @@
-import { Fill } from "../../models";
+import { Fill, GameState } from "../../models";
 import { Tetris } from "../Tetris";
 
-jest.setTimeout(3000);
+jest.setTimeout(5000);
 
 describe("Game logic", () => {
     it("should move current piece down automatically", (done) => {
@@ -111,14 +111,94 @@ describe("Game logic", () => {
         }
     });
 
-    // xit("should rotate current piece left when there is space");
-    // xit("should rotate current piece right when there is space");
+    it("should rotate current piece left when there is space", () => {
+        const pieceGenerator = () => [[Fill.Blue, Fill.Blank], [Fill.Blank, Fill.Blank]];
+        const tetris = new Tetris(pieceGenerator);
+        tetris.start();
+        tetris.rotateLeft();
+        expect(tetris.getState().piece).toEqual([[Fill.Blank, Fill.Blank], [Fill.Blue, Fill.Blank]]);
+    });
+
+    it("should rotate current piece right when there is space", () => {
+        const pieceGenerator = () => [[Fill.Blue, Fill.Blank], [Fill.Blank, Fill.Blank]];
+        const tetris = new Tetris(pieceGenerator);
+        tetris.start();
+        tetris.rotateRight();
+        expect(tetris.getState().piece).toEqual([[Fill.Blank, Fill.Blue], [Fill.Blank, Fill.Blank]]);
+    });
+
     // xit("should be unable to rotate left when there is no space");
     // xit("should be unable to rotate right when there is no space");
-    // xit("should generate a new piece when current piece completes drop");
+
+    it("should generate a new piece when current piece completes drop", async () => {
+        let i = 0;
+        const pieceGenerator = () => {
+            i++;
+            if (i === 1) {
+                return [[Fill.Green]];
+            } else if (i === 2) {
+                return [[Fill.Orange]];
+            } else if (i === 3) {
+                return [[Fill.Blue]];
+            } else {
+                return [[Fill.Gray]];
+            }
+        };
+        const tetris = new Tetris(pieceGenerator);
+        tetris.start();
+        expect(tetris.getState().piece).toEqual([[Fill.Green]]);
+        expect(tetris.getState().nextPiece).toEqual([[Fill.Orange]]);
+        await tetris.drop(true);
+        expect(tetris.getState().piece).toEqual([[Fill.Orange]]);
+        expect(tetris.getState().nextPiece).toEqual([[Fill.Blue]]);
+    });
+
     // xit("should be able to hard drop a piece");
-    // xit("should end the game when there is an immediate collision from a newly generated piece");
-    // xit("should be able to restart the game after game over");
+    // xit("should be able to soft drop a piece");
+
+    it("should end the game when there is an immediate collision from a newly generated piece", async () => {
+        const tetris = new Tetris();
+        tetris.start();
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        // more than 10 pieces dropped without manuever should be sufficient to end the game
+        for (let i = 0; i < 15; i++) {
+            await tetris.drop(true);
+        }
+        expect(tetris.getState().gameState).toBe(GameState.GameOver);
+    });
+
+    it("should be able to pause an active game", async () => {
+        const tetris = new Tetris();
+        tetris.start();
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        await tetris.drop(true);
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        tetris.togglePause();
+        expect(tetris.getState().gameState).toBe(GameState.Paused);
+        const prevPosition = tetris.getState().position;
+        const prevPiece = tetris.getState().piece;
+        await new Promise((res, rej) => setTimeout(res, 2000));
+        expect(prevPosition).toEqual(tetris.getState().position);
+        expect(prevPiece).toEqual(tetris.getState().piece);
+        tetris.togglePause();
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        await new Promise((res, rej) => setTimeout(res, 2000));
+        expect(prevPosition).not.toEqual(tetris.getState().position);
+    });
+
+    it("should be able to restart the game after game over", async () => {
+        const tetris = new Tetris();
+        tetris.start();
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        // more than 10 pieces dropped without manuever should be sufficient to end the game
+        for (let i = 0; i < 15; i++) {
+            await tetris.drop(true);
+        }
+        expect(tetris.getState().gameState).toBe(GameState.GameOver);
+        tetris.restart();
+        expect(tetris.getState().gameState).toBe(GameState.Active);
+        expect(tetris.getState().playfield.filter((line) => line.every((cell) => !!cell))).toEqual([]);
+    });
 });
 
 xdescribe("Scoreboard", () => {
@@ -153,7 +233,6 @@ const completeLine = async (tetris: Tetris) => {
 
     tetris.moveLeft();
     await tetris.drop(true);
-
 
     tetris.moveRight();
     await tetris.drop(true);
